873875243677d77a78f209a81e900df437861c20
gcdesigner <gcdesigner.contato@gmail.com>
2024-08-29 21:48:51 -0300

    start next project with tailwind and shadcdn

---
+++ wemovie-ecommerce/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wefit-react-web-test.s3.amazonaws.com'
+      }
+    ]
+  }
+};
+
+export default nextConfig;
